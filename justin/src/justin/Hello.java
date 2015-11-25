8bbfa4bf7f151feeb8f3353fa690d358208cced4
Justin Altena <psvjustin@gmail.com>
2015-11-25 11:25:17 +0100

    code veranderd

---
+++ justin/src/justin/Hello.java
@@ -0,0 +1,17 @@
+package justin;
+
+public class Hello {
+	
+	private int legs;
+	private boolean hasTail;
+	private String color;
+	private boolean awake;
+	
+	public Hello (int legs, boolean hasTail, String color, boolean awake) {
+		this.legs = legs;
+		this.hasTail = hasTail;
+		this.color = color;
+		this.awake = awake; 
+	}
+
+}
