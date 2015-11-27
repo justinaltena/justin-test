833f34e6da151e0e09f3d0b39eab80472e648caf
Justin Altena <psvjustin@gmail.com>
2015-11-27 10:21:03 +0100

    Hello class aangepast

--- justin/src/justin/Hello.java
+++ justin/src/justin/Hello.java
@@ -3,15 +3,19 @@ package justin;
 public class Hello {
 	
 	private int legs;
-	private boolean hasTail;
-	private String color;
-	private boolean awake;
+
 	
 	public Hello (int legs, boolean hasTail, String color, boolean awake) {
 		this.legs = legs;
-		this.hasTail = hasTail;
-		this.color = color;
-		this.awake = awake; 
+
+	}
+	
+	public int getLegs() {
+		return legs;
+	}
+	
+	public void setLegs(int newLegs) {
+		this.legs = newLegs;
 	}
 
 }
--- justin/src/justin/Test.java
+++ justin/src/justin/Test.java
@@ -1,5 +1,7 @@
 package justin;
 
 public class Test {
+	
+	
 
 }
